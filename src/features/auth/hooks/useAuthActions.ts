039b05d7346e61c1dbb1ab88263a525493dece4e
hwanheejung <hwanheejung117@gmail.com>
2024-06-05 13:48:34 +0900

    #1 fix: jwt 로직 수정

--- src/features/auth/hooks/useAuthActions.ts
+++ src/features/auth/hooks/useAuthActions.ts
@@ -1,10 +1,36 @@
-// { signin,signout, signup }
-import axios, { AxiosResponse } from "axios";
+import { signout } from "@/utils/auth";
+import axios from "axios";
 
 interface IUseAuth {
   signin: (email: string, password: string) => Promise<void>;
-  signup: (email: string, password: string) => Promise<void>;
+  signup: (email: string, password: string, username: string) => Promise<void>;
   signout: () => void;
 }
 
-export function useAuthActions(): IUseAuth {}
+export const useAuthActions = (): IUseAuth => {
+  const signup = async (email: string, password: string, username: string) => {
+    try {
+      const response = await axios.post("/auth/signup", {
+        email,
+        password,
+        username,
+      });
+
+      console.log("Signup successful:", response.data);
+    } catch (error) {
+      console.error("Signup error:", error);
+    }
+  };
+
+  const signin = async (email: string, password: string) => {
+    try {
+      const response = await axios.post("/auth/login", { email, password });
+      const accessToken = response.headers.authorization.split(" ")[1];
+      localStorage.setItem("accessToken", accessToken);
+    } catch (error) {
+      console.error("Login error:", error);
+    }
+  };
+
+  return { signin, signup, signout };
+};
