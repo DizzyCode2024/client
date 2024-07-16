0988e1896bb6af9caf85f60b2603210841b8b67b
minkyung_kwak <ellen10min@gmail.com>
2024-07-16 15:56:30 +0900

    #88 Style: NoChatUI변경

--- src/components/chat/ChatBody/NoChat.tsx
+++ src/components/chat/ChatBody/NoChat.tsx
@@ -10,26 +10,25 @@ const NoChatUI = () => {
         ml={5}
         mb={5}
         bg={'gray.500'}
-        width={'8rem'}
-        height={'8rem'}
+        width={'5rem'}
+        height={'5rem'}
         borderRadius={'50%'}
         display={'flex'}
         justifyContent={'center'}
         alignItems={'center'}
       >
-        <ChatIcon width={'3rem'} height={'3rem'} />
+        <ChatIcon width={'2rem'} height={'2rem'} />
       </Box>
 
       <Box
-        fontSize={'5xl'}
+        fontSize={'3xl'}
         display={'flex'}
         alignItems={'center'}
         fontWeight={'bold'}
       >
-        <ChatIcon width={'2rem'} mr={5} />
         {`${name}에 오신 걸 환영 합니다!`}
       </Box>
-      <Box fontSize={'2xl'} display={'flex'} alignItems={'center'}>
+      <Box fontSize={'lg'} display={'flex'} alignItems={'center'}>
         <ChatIcon mr={2} />
         {`${name} 채팅 채널의 시작이에요.`}
       </Box>
