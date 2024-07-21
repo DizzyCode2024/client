fe9cf685d780e9c6be0ce9c3d0e7174fdfa767be
minkyung_kwak <ellen10min@gmail.com>
2024-07-21 19:55:24 +0900

    #101 Add: dmSection 구축

--- src/components/dm/index.tsx
+++ src/components/dm/index.tsx
@@ -1,6 +1,29 @@
-import { Box } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
+import { useState } from 'react';
+import Header from '../chat/ChatHeader/Header';
+import ChatContainer from '../chat/ChatBody/ChatContainer';
+import DmInput from './DmInput/DmInput';
+import MemberList from '../memberList';
+import Container from '../chat/DragFileContainer';
 
-const DmSection = () => {
-  return <Box>{'dm 부분'}</Box>;
+const DMSection = () => {
+  const [isMembersOpen, setIsMembersOpen] = useState<boolean>(false);
+
+  return (
+    <Container>
+      <Header
+        isMembersOpen={isMembersOpen}
+        setIsMembersOpen={setIsMembersOpen}
+      />
+      <Flex flex={1}>
+        <Flex flex={1} direction={'column'}>
+          <ChatContainer />
+          <DmInput />
+        </Flex>
+      </Flex>
+      <Flex>{isMembersOpen && <MemberList />}</Flex>
+    </Container>
+  );
 };
-export default DmSection;
+
+export default DMSection;
